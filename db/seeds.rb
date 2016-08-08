bfa6255f16b6dc3023ec5f8a385d6d1ddd1b98b7
Schyler Hosler <schylerhosler@gmail.com>
2016-08-08 17:14:23 -0400

    Initial commit:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+desc 'migrate changes to your database'
+namespace :db do
+  task :migrate => :environment do
+    Student.create_table
+  end
+end
